5d5d39cdaca83913f15e9a6bbfa056fdc6f4fd63
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-08-07 22:02:50 +0900

    #13 - query function

--- app/core/database.php
+++ app/core/database.php
@@ -7,8 +7,26 @@ class Database
     $str = DB_DRIVER . ":hostname=" . DB_HOST . "dbname=" . DB_NAME;
     return new PDO($str, DB_USER, DB_PW);
   }
-  public function query()
+
+  public function query($query, $data = [], $type = 'obj')
   {
     $con = $this->connect();
+
+    $stm = $con->prepare($query);
+    if ($stm) {
+      $check = $stm->execute($data);
+      if ($check) {
+        $type = PDO::FETCH_OBJ;
+        if ($type != 'obj') {
+          $type = PDO::FETCH_ASSOC;
+        }
+        $result = $stm->fetchAll($type);
+
+        if (is_array($result) && count($result) > 0) {
+          return $result;
+        }
+      }
+    }
+    return false;
   }
 }
