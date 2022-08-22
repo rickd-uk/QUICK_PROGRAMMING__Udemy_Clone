e91fbcdbf0cde924bde245970534a30f9a786424
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-08-22 21:57:03 +0900

    #85 - home courses 2

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -22,15 +22,18 @@ class Home extends Controller
     $data['rows'] = $course->where(['approved' => 0], 'DESC', '', 7);
 
     if ($data['rows']) {
-      $data['first_r'] = $data['rows'][0];
+      $data['first_row'] = $data['rows'][0];
+      // Remove first post so it won't be repeated
+      unset($data['rows'][0]);
 
       $total_rows = count($data['rows']);
       $half_rows = round($total_rows / 2);
 
-      $data['rows1'] = array_splice($data['rows'], 0, $half_rows);
-      $data['rows2'] = array_splice($data['rows'], $half_rows, $total_rows);
 
-      show($total_rows);
+
+      $data['rows1'] = array_splice($data['rows'], 0, $half_rows);
+      $data['rows2'] = $data['rows'];
+      // ss($data['rows']);
     }
 
 
--- app/views/home.view.php
+++ app/views/home.view.php
@@ -66,7 +66,7 @@ $this->view('includes/nav', $data);
   </div>
 </section><!-- End Hero Slider Section -->
 
-<?php if (!empty($rows)) :  ?>
+<?php if (!empty($first_row)) :  ?>
   <!-- ======= Post Grid Section ======= -->
   <section id="posts" class="posts">
     <div class="container" data-aos="fade-up">
@@ -75,17 +75,17 @@ $this->view('includes/nav', $data);
 
 
           <div class="post-entry-1 lg">
-            <a href="single-post.html"><img src="<?= get_image($first_r->course_image, 'courses') ?>" alt="" class="img-fluid" style="object-fit:cover"></a>
-            <div class="post-meta"><span class="date">Culture</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
-            <h2><a href="single-post.html"><?= esc($first_r->title) ?></a></h2>
-            <p class="mb-4 d-block"><?= esc($first_r->description)  ?></p>
+            <a href="single-post.html"><img src="<?= get_image($first_row->course_image, 'courses') ?>" alt="" class="img-fluid" style="object-fit:cover"></a>
+            <div class="post-meta"><span class="date">Culture</span> <span class="mx-1">&bullet;</span> <span><?= get_date($first_row->date) ?></span></div>
+            <h2><a href="single-post.html"><?= esc($first_row->title) ?></a></h2>
+            <p class="mb-4 d-block"><?= esc($first_row->description)  ?></p>
 
-            <?php if (!empty($first_r->user_row)) : ?>
+            <?php if (!empty($first_row->user_row)) : ?>
               <div class="d-flex align-items-center author">
-                <div class="photo"><img src="<?= get_image($first_r->user_row->image) ?>" alt="" class="img-fluid"></div>
+                <div class="photo"><img src="<?= get_image($first_row->user_row->image) ?>" alt="" class="img-fluid"></div>
                 <div class="name">
 
-                  <h3 class="<?= $first_r->user_row->firstname  . ' ' . $first_r->user_row->lastname  ?>"> </h3>
+                  <h3 class="<?= $first_row->user_row->firstname  . ' ' . $first_row->user_row->lastname  ?>"> </h3>
                 </div>
               </div>
             <?php endif; ?>
@@ -96,23 +96,27 @@ $this->view('includes/nav', $data);
         <div class="col-lg-8">
           <div class="row g-5">
             <div class="col-lg-4 border-start custom-border">
-              <?php foreach ($rows1 as $row) : ?>
-                <div class="post-entry-1">
-                  <a href="single-post.html"><img src="<?= get_image($row->course_image, 'courses') ?>" alt="" class="img-fluid"></a>
-                  <div class="post-meta"><span class="date"><?= esc($row->category_row->category ?? 'Unknown') ?></span> <span class="mx-1">&bullet;</span> <span><?= get_date($row->date) ?? 'No date' ?></span></div>
-                  <h2><a href="single-post.html">Let’s Get Back to Work, New York</a></h2>
-                </div>
-              <?php endforeach; ?>
+              <?php if (!empty($rows1)) : ?>
+                <?php foreach ($rows1 as $row) : ?>
+                  <div class="post-entry-1">
+                    <a href="single-post.html"><img src="<?= get_image($row->course_image, 'courses') ?>" alt="" class="img-fluid"></a>
+                    <div class="post-meta"><span class="date"><?= esc($row->category_row->category ?? 'Unknown') ?></span> <span class="mx-1">&bullet;</span> <span><?= get_date($row->date) ?? 'No date' ?></span></div>
+                    <h2><a href="single-post.html">GREAT</a></h2>
+                  </div>
+                <?php endforeach; ?>
+              <?php endif; ?>
             </div>
 
             <div class="col-lg-4 border-start custom-border">
-              <?php foreach ($rows2 as $row) : ?>
-                <div class="post-entry-1">
-                  <a href="single-post.html"><img src="<?= get_image($row->course_image, 'courses') ?>" alt="" class="img-fluid"></a>
-                  <div class="post-meta"><span class="date"><?= esc($row->category_row->category ?? 'Unknown') ?></span> <span class="mx-1">&bullet;</span> <span><?= get_date($row->date) ?? 'No date' ?></span></div>
-                  <h2><a href="single-post.html">Let’s Get Back to Work, New York</a></h2>
-                </div>
-              <?php endforeach; ?>
+              <?php if (!empty($rows2)) : ?>
+                <?php foreach ($rows2 as $row) : ?>
+                  <div class="post-entry-1">
+                    <a href="single-post.html"><img src="<?= get_image($row->course_image, 'courses') ?>" alt="" class="img-fluid"></a>
+                    <div class="post-meta"><span class="date"><?= esc($row->category_row->category ?? 'Unknown') ?></span> <span class="mx-1">&bullet;</span> <span><?= get_date($row->date) ?? 'No date' ?></span></div>
+                    <h2><a href="single-post.html">Let’s Get Back to Work, New York</a></h2>
+                  </div>
+                <?php endforeach; ?>
+              <?php endif; ?>
 
 
             </div>
