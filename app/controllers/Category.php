16405c4a5afbfb8d72ec8c4b42156a982a9f122a
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-08-25 21:51:06 +0900

    #97 - categories query

--- app/controllers/Category.php
+++ app/controllers/Category.php
@@ -2,6 +2,8 @@
 
 namespace Controller;
 
+use \Model\Course;
+
 
 if (!defined("ROOT")) die("direct script access denied");
 
@@ -9,31 +11,30 @@ if (!defined("ROOT")) die("direct script access denied");
  * category class
  */
 
-
 class Category extends Controller
 {
-  public function index()
+  public function index($slug = null)
   {
-    // $ad = new Add_Data();
-    // $ad->to('test');
-    $data['title'] = "Category";
-
+    $course = new Course();
     $category = new \Model\Category();
 
-    $data['rows'] = $category->query("SELECT c.*, category, cat.slug as cat_slug FROM `courses` AS c JOIN categories AS cat ON cat.id = c.category_id WHERE cat.slug = 'it-software'");
+    $data['title'] = "Category";
+
+    $query = "SELECT c.*, category, cat.slug as cat_slug FROM `courses` AS c JOIN categories AS cat ON cat.id = c.category_id WHERE cat.slug = :slug";
+    $data['rows'] = $category->query($query, ['slug' => $slug]);
 
 
-    // if ($data['rows']) {
-    //   $data['first_row'] = $data['rows'][0];
-    //   // Remove first post so it won't be repeated
-    //   unset($data['rows'][0]);
+    if ($data['rows']) {
+      $data['first_row'] = $data['rows'][0];
+      // Remove first post so it won't be repeated
+      unset($data['rows'][0]);
 
-    //   $total_rows = count($data['rows']);
-    //   $half_rows = round($total_rows / 2);
+      $total_rows = count($data['rows']);
+      $half_rows = round($total_rows / 2);
 
-    //   $data['rows1'] = array_splice($data['rows'], 0, $half_rows);
-    //   $data['rows2'] = $data['rows'];
-    // }
+      $data['rows1'] = array_splice($data['rows'], 0, $half_rows);
+      $data['rows2'] = $data['rows'];
+    }
 
 
     $this->view('category', $data);
